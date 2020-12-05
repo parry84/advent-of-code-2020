53c210aff1e552bff61e21724216be13e06e0691
Emanuele Parrinello <parry84@gmail.com>
2020-12-06 00:09:56 +0100

    🤶 Add Day 3 solution

--- src/AOC/Challenge/Day03.hs
+++ src/AOC/Challenge/Day03.hs
@@ -1,6 +1,3 @@
-{-# OPTIONS_GHC -Wno-unused-imports   #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 -- |
 -- Module      : AOC.Challenge.Day03
 -- License     : BSD3
@@ -9,35 +6,33 @@
 -- Portability : non-portable
 --
 -- Day 3.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
-module AOC.Challenge.Day03 (
-    -- day03a
-  -- , day03b
-  ) where
+module AOC.Challenge.Day03 (day03a, day03b) where
 
 import           AOC.Prelude
 
-day03a :: _ :~> _
+type Slope = (Int, Int)
+type Grid  = [String]
+
+day03a :: Grid :~> Int
 day03a = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . trees [(3, 1)]
     }
 
-day03b :: _ :~> _
+day03b :: Grid :~> Int
 day03b = MkSol
-    { sParse = Just
+    { sParse = Just . lines
     , sShow  = show
-    , sSolve = Just
+    , sSolve = Just . trees [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     }
+
+trees :: [Slope] -> Grid -> Int
+trees slopes grid = product $ fmap (`countTrees` grid) slopes
+
+countTrees :: Slope -> Grid -> Int
+countTrees (dx, dy) grid = countTrue (=='#') [(grid !! (i * dy)) !! (i * dx `mod` width) | i <- [1 .. height], i * dy < height]
+  where
+    width = length $ head grid
+    height = length grid 
